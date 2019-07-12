b48ac6ba4d96d5d78cd4d6744b90e0a75e40fbb1
WildanKurniadi <danprogramming10@gmail.com>
2019-07-13 01:34:34 +0700

    #06 Menggunakan template header dan footer pada view yang dipnggil melalui controller

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,13 @@
 <?php
 
-class About extends Controller{
+class About extends Controller
+{
 
    /**
     * "index" merupakan method default, 
     * Kita perlu membuat method index, agar ketika controller About dipanggil tanpa ada method PHP tidak akan mengarahkan kita ke route default
     */
-   public function index($nama = 'Wildan K', $pekerjaan='Sultan',$usia=24) //berikan nilai default jika $nama dan $pekerjaan kosong
+   public function index($nama = 'Wildan K', $pekerjaan = 'Sultan', $usia = 24) //berikan nilai default jika $nama dan $pekerjaan kosong
    {
       //gunakan array associative untuk mengirimkan array $data
       $data['nama'] = $nama;
@@ -14,9 +15,12 @@ class About extends Controller{
       $data['usia'] = $usia;
 
       //kirimkan $data sebagai argument kedua saat memanggil method view
-     $this->view('about/index',$data);
+      $this->view('templates/header', $data);
+      $this->view('about/index', $data);
+      $this->view('templates/footer', $data);
    }
-   public function page(){
+   public function page()
+   {
       $this->view('about/page');
    }
 }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,20 +1,2 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>About/Index</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="">
-   </head>
-   <body>
-   <h1>About Me</h1>
-      <p>Hallo nama saya <?php echo $data['nama']?>, umur saya <?php echo $data['usia']?>, pekerjaan saya adalah <?php echo $data['pekerjaan']?></p>
-   
-      <script src="" async defer></script>
-   </body>
-</html>
+<h1>About Me</h1>
+<p>Hallo nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['usia'] ?>, pekerjaan saya adalah <?php echo $data['pekerjaan'] ?></p>
