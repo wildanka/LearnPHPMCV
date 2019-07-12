369e5890f5872e469c3bb57cc576f69f45246558
WildanKurniadi <danprogramming10@gmail.com>
2019-07-13 02:18:22 +0700

    #10 now we simply using assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,5 @@
 <div class="container">
    <h1>About Me</h1>
+   <img src="<?= BASE_URL . '/img/profile.jpg' ?>" alt="profile_picutre" class="rounded-circle shadow" width="200" height="170">
    <p>Hallo nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['usia'] ?>, pekerjaan saya adalah <?php echo $data['pekerjaan'] ?></p>
 </div>
