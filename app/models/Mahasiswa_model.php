76c7cc66809413da429a83601610511d3edc595b
WildanKurniadi <danprogramming10@gmail.com>
2019-07-13 23:10:08 +0700

    #15 Mengambil data dari database menggunakan PDO (PHP Data Object) bukan mysqli, dan menyimpannya di model (Mahasiswa_model.php)

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,29 +1,32 @@
 <?php
 class Mahasiswa_model
 {
-   private $mhs = [
-      [
-         "nama" => "Wildan Kurniadi",
-         "nim" => "10113211",
-         "email" => "danprogramming10@gmail.com",
-         "jurusan" => "Teknik Informatika"
-      ],
-      [
-         "nama" => "Arief Hidayat",
-         "nim" => "10113222",
-         "email" => "arief.hidayat@gmail.com",
-         "jurusan" => "Teknik Informatika"
-      ],
-      [
-         "nama" => "Anindya Khoirunnisa",
-         "nim" => "10113233",
-         "email" => "anindyadesign@gmail.com",
-         "jurusan" => "Desain Komunikasi Visual"
-      ]
-   ];
+   //konek ke database menggunakan driver PDO (PHP Data Object), sehingga memudahkan kita ketika mengganti database, misalnya jika kita tidak ingin menggunakan MySQL lagi (MongoDB, Postgre, dll.)
+   private $dbh; //database helper
+   private $stmt; //database statement
+
+   /**
+    * Lakukan koneksi ke database di dalam method __construct sehingga ketika model ini dipanggil akan membuka koneksi ke database.
+    */
+   public function __construct()
+   {
+      //sekarang kita melakukan koneksi menggunakan cara paling mudah. (jangan digunakan untuk produksi, jangan simpan username, password, dan nama DB kalian di file ini) sebaiknya simpan di file lain supaya tidak mudah diakses oleh orang yang tidak berhak
+      //datasource name
+      $dsn = 'mysql:host=localhost;dbname=belajarphpmvc;';
+
+      try { //coba membuat koneksi
+         $this->dbh = new PDO($dsn, 'root', '');
+      } catch (PDOException $e) {
+         //jika terjadi error hentikan program dan tampilkan pesan Errornya
+         die($e->getMessage());
+      }
+   }
 
    public function getMhs()
    {
-      return $this->mhs;
+      $this->stmt = $this->dbh->prepare('SELECT * FROM t_mahasiswa');
+      $this->stmt->execute();
+      //jika pada driver mysql adalah mysqli_fetch_assoc, maka dengan driver PDO cara kita mendapatkan hasil eksekusi query adalah sebagai berikut.
+      return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
    }
 }
