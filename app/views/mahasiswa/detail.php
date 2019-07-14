ca257f4705c9c1727e8b36e6ec5fa5da05c3002a
WildanKurniadi <danprogramming10@gmail.com>
2019-07-14 16:57:32 +0700

    #18 UI Refinement

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -1,4 +1,13 @@
-<div class="container">
+<div class="container mt-5">
 
-   <?= $data['mhs']['nama']; ?>
+   <div class="card" style="width: 18rem;">
+      <div class="card-body">
+         <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
+         <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nim']; ?></h6>
+         <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
+         <p class="card-text"><?= $data['mhs']['email']; ?></p>
+         <a href="<?= BASE_URL . '/mahasiswa' ?>" class="card-link">Kembali</a>
+      </div>
+   </div>
+
 </div>
