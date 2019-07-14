5760ced0d393f8bd343dbad29cc0ecfd4badfdc1
WildanKurniadi <danprogramming10@gmail.com>
2019-07-14 17:12:21 +0700

    #19 Penambahan UI untuk Tambah Data Mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,6 +1,11 @@
 <div class="container mt-5">
    <div class="row">
       <div class="col-6">
+         <!-- Button trigger modal -->
+         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#tambahMahasiswaModal">
+            Tambah Data Mahasiswa
+         </button> <br><br>
+
          <h3>Daftar Mahasiswa</h3>
 
          <?php foreach ($data['mahasiswa'] as $mhs) : ?>
@@ -15,14 +20,51 @@
 
          <?php endforeach; ?>
 
-         <!-- 
-            <ul>
-               <li><?= $mhs[' nama '] ?></li>
-               <li><?= $mhs[' nim '] ?></li>
-               <li><?= $mhs[' jurusan '] ?></li>
-               <li><?= $mhs[' email'] ?></li>
-            </ul>
-          -->
+      </div>
+   </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="tambahMahasiswaModal" tabindex="-1" role="dialog" aria-labelledby="tambahMahasiswaModalLabel" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="tambahMahasiswaModalLabel">Tambah Data Mahasiswa</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="modal-body">
+            <form action="" method="POST">
+               <div class="form-group">
+                  <label for="nama">Nama</label>
+                  <input type="text" class="form-control" id="nama" aria-describedby="name" placeholder="Masukkan nama mahasiswa">
+               </div>
+               <div class="form-group">
+                  <label for="nim">NIM</label>
+                  <input type="text" class="form-control" id="nim" aria-describedby="nim" placeholder="Masukkan NIM">
+               </div>
+               <div class="form-group">
+                  <label for="email">Email address</label>
+                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Masukkan Email">
+                  <small id="emailHelp" class="form-text text-muted">Kami tidak akan membagikan alamat email Anda kepada orang lain.</small>
+               </div>
+               <div class="form-group">
+                  <label for="jurusan">Jurusan</label>
+                  <select class="form-control" id="jurusan">
+                     <option>Teknik Informatika</option>
+                     <option>Teknik Komputer</option>
+                     <option>Sistem Informasi</option>
+                     <option>Desain Interior</option>
+                     <option>Desain Komunikasi Visual</option>
+                  </select>
+               </div>
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            </form>
+         </div>
       </div>
    </div>
 </div>
